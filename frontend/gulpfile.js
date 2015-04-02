de470dcbf93d57ce79b022b86a8ce6ecc97ac17b
Kevin Simper <kevin.simper@gmail.com>
2015-04-02 19:57:17 +0200

    Begin admin app on frontend

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -16,10 +16,26 @@ gulp.task('browserify', function() {
   return gulp.src(['./app/index.js'])
     .pipe(browserified)
     .pipe(rename('bundle.js'))
-    .pipe(gulp.dest('public/build/'))
+    .pipe(gulp.dest('public/build/'));
 });
 
-gulp.task('build', ['browserify']);
-gulp.task('default', ['browserify'], function() {
-  gulp.watch(['./app/**'], ['browserify']);
+gulp.task('browserify-admin', function() {
+  var b = browserify()
+    .transform(reactify);
+
+  var browserified = transform(function(filename) {
+    b.add(filename);
+    return b.bundle();
+  });
+
+  return gulp.src(['./app/admin/index.js'])
+    .pipe(browserified)
+    .pipe(rename('admin.js'))
+    .pipe(gulp.dest('public/build/'));
+});
+
+gulp.task('build', ['browserify', 'browserify-admin']);
+gulp.task('default', ['browserify', 'browserify-admin'], function() {
+  gulp.watch(['./app/**', './app/admin'], ['browserify']);
+  gulp.watch(['./app/admin/**'], ['browserify-admin']);
 });
