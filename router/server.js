13c9f4605f8c085ae2b84015c649695ddfbeb840
James Brooking <jambroo@gmail.com>
2015-04-16 17:54:42 +0000

    Merging router-work branch

--- router/server.js
+++ router/server.js
@@ -6,22 +6,32 @@ server.connection({
     port: '8500'
 });
 
+// Had to add hosts file entry to test this as:
+// 88.80.187.61 jambroo.dev.jambroo.com
+// TODO: Register domain and handle subdomains properly
+
 var mapper = function (request, callback) {
+    var requestedHostname = request.info.host;
+    var requestedHostnameSplit = requestedHostname.split('.');
+    if (requestedHostnameSplit && requestedHostnameSplit.length) {
+        var requestedCname = requestedHostnameSplit[0];
+    } else {
+        throw 'No subdomain in request.';
+    }
+
     var url = 'mongodb://localhost:27017/blackbeard';
 
     // Use connect method to connect to the `
     MongoClient.connect(url, function(err, db) {
         var collection = db.collection('apps');
 
-        var requestedCname = request.url.path.substr(1, request.url.path.length);
-
         collection.findOne({cname: requestedCname}, function(error, result) {
-            var uri = '';
             if (result) {
-                uri = "http://"+result.ip+":"+result.port;
+                var uri = "http://"+result.ip+":"+result.port;
+                callback(null, uri);
+            } else {
+                throw 'No app found matching CNAME.';
             }
-            
-            callback(null, uri);
         })
     });
 }
