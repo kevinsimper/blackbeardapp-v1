e7a2a385feb351f3b6da0f999765934fbd304570
Kevin Simper <kevin.simper@gmail.com>
2015-04-18 22:55:37 +0200

    Frontend post route

--- frontend/server.js
+++ frontend/server.js
@@ -2,7 +2,15 @@ var express = require('express');
 var harp = require('harp');
 var app = express();
 
+var BACKEND_API = process.env.BACKEND_API;
+
 app.use(harp.mount(__dirname + '/public'));
+
+app.post('/signup', function(req, res) {
+  res.json({
+    status: 'Fine'
+  });
+});
 
 var port = process.env.PORT || 9000
 app.listen(port, function() {
