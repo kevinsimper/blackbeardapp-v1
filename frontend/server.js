e1a9057514fc42642155dc0090519b8f56217d01
Kevin Simper <kevin.simper@gmail.com>
2015-04-18 22:00:55 +0200

    Mount harp as a middleware instead

--- frontend/server.js
+++ frontend/server.js
@@ -1,1 +1,10 @@
-require('harp').server(__dirname + '/public', { port: process.env.PORT || 9000 })
+var express = require('express');
+var harp = require('harp');
+var app = express();
+
+app.use(harp.mount(__dirname + '/public'));
+
+var port = process.env.PORT || 9000
+app.listen(port, function() {
+  console.log('Frontend started listening on ' + port);
+});
