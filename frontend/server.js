6cb2c1157e2b1ccd218e93a5789ae58becbc315f
Kevin Simper <kevin.simper@gmail.com>
2015-04-19 15:01:05 +0200

    Define host be consistent

--- frontend/server.js
+++ frontend/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var harp = require('harp');
 var app = express();
 
-var BACKEND_API = process.env.BACKEND_API;
+var BACKEND_HOST = process.env.BACKEND_HOST;
 
 app.use(harp.mount(__dirname + '/public'));
 
