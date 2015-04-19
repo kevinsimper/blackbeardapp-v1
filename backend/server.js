f608c5a4493154517bc44ad80014753bf16fcf68
Kevin Simper <kevin.simper@gmail.com>
2015-04-19 14:50:34 +0200

    Set up to use docker link in development

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 var Hapi = require('hapi'),
     MongoClient = require('mongodb').MongoClient;
 
-var server = new Hapi.Server({ connections: { routes: { cors: true } } });
-server.connection({ port: '8000' });
+var server = new Hapi.Server();
+server.connection({
+    port: '8000'
+});
+
+console.log('Mode:', process.env.NODE_ENV);
+
+var DATABASE_URL = 'mongodb://' + process.env.DB_PORT_27017_TCP_ADDR + ':' +process.env.DB_PORT_27017_TCP_PORT + '/blackbeard';
+
 
 server.route({
     method: 'GET',
@@ -16,31 +23,29 @@ server.route({
     method: 'POST',
     path: '/signup',
     handler: function(request, reply) {
-        var url = 'mongodb://localhost:27017/blackbeard';
 
-        // Use connect method to connect to the `
-        MongoClient.connect(url, function(err, db) {
+        MongoClient.connect(DATABASE_URL, function(err, db) {
+            if(err) return console.log(err);
+
             var collection = db.collection('users');
             // Insert new user
             var email = request.payload.email;
             collection.insert({
                 email: email,
                 active: false,
-                timestamp: Math.round(Date.now()/1000)
+                timestamp: Math.round(Date.now() / 1000)
             }, function(err, result) {
                 if (err) {
                     reply('error').code(500)
                 } else {
                     reply('ok')
                 }
-                
+
                 db.close();
             });
         });
     }
 });
-
-
 
 server.start(function() {
     console.log('Server running at:', server.info.uri);
