63c81b203f43bde2b473586511839b0164ad6c34
James Brooking <jambroo@gmail.com>
2015-04-20 21:58:04 +0000

    Added /admin/user action.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 var Hapi = require('hapi'),
     MongoClient = require('mongodb').MongoClient,
+    ObjectID = require('mongodb').ObjectID,
     passwordHash = require('password-hash');
  
 var server = new Hapi.Server({ connections: { routes: { cors: true } } });
@@ -138,6 +139,57 @@ server.route({
 
                 db.close();
               });
+        });
+    }
+});
+
+// Admin
+// Login
+server.route({
+    method: 'PUT',
+    path: '/admin/user',
+    handler: function(request, reply) {
+        MongoClient.connect(DATABASE_URL, function(err, db) {
+            if(err) return console.log(err);
+
+            var collection = db.collection('users_soon');
+
+            var adminHash = request.payload.admin;
+            if (adminHash != 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855') {
+                reply('invalid_admin_hash')
+            } else {
+                var userHash = request.payload.userHash;
+                var email = request.payload.email;
+                var password = request.payload.password;
+
+                collection.find({
+                    _id: ObjectID(userHash)
+                }).toArray(function(err, docs) {
+                    if (docs.length > 0) {
+                        var user = docs[0];
+                        var hashedPassword = passwordHash.generate(password);
+
+                        collection.update(
+                            {_id: ObjectID(userHash)},
+                            { $set: { email: email, password_hashed: hashedPassword } },
+                            function(err, result) {
+                                if (err) {
+                                    reply(result)
+                                } else {
+                                    reply('ok')
+                                }
+                            }
+                        );
+
+                    } else {
+                        reply('user_not_found');
+                    }
+
+                    db.close();
+                });
+            }
+
+            
         });
     }
 });
