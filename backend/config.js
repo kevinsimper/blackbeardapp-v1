eceb2e6609265ea3db79ba79a5a6570eed3ed967
Kevin Simper <kevin.simper@gmail.com>
2015-05-14 13:33:34 +0200

    if env is production

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,5 @@
-if (!process.env.DB_PORT_27017_TCP_ADDR) {
-  process.env.DB_PORT_27017_TCP_ADDR = 'localhost';
-  process.env.DB_PORT_27017_TCP_PORT = 27017;
+if(process.env.NODE_ENV == 'production') {
+  exports.DATABASE_URL = 'mongodb://blackbeard:dgf4e6lyGs6Ck7jwEkcXKC73JJY@ds037611.mongolab.com:37611/blackbeard'; 
+} else {
+  exports.DATABASE_URL = 'mongodb://' + process.env.DB_PORT_27017_TCP_ADDR + ':' +process.env.DB_PORT_27017_TCP_PORT + '/blackbeard';
 }
-
-// exports.DATABASE_URL = 'mongodb://' + process.env.DB_PORT_27017_TCP_ADDR + ':' +process.env.DB_PORT_27017_TCP_PORT + '/blackbeard';
-exports.DATABASE_URL = 'mongodb://db.blackbeard.io:27017/blackbeard';
