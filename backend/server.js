43c77366dabb1afc008e882e636c737d3947cea2
James Brooking <jambroo@gmail.com>
2015-05-14 12:54:18 +0200

    Env variable test commit.

--- backend/server.js
+++ backend/server.js
@@ -78,6 +78,14 @@ server.route({
   handler: userRoutes.postLogin
 });
 
+server.route({
+    method: 'GET',
+    path: '/test_env',
+    handler: function(request, reply) {
+        reply(process.env.CIRCLECI)
+    }
+})
+
 server.start(function() {
   console.log('Server running at:', server.info.uri);
 });
