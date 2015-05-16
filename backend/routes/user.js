d8cc902dd855b882b0158aa06e7e169a9e184705
Kevin Simper <kevin.simper@gmail.com>
2015-05-16 09:54:50 +0200

    Use User dao object

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,51 +2,42 @@ var MongoClient = require('mongodb').MongoClient
 var ObjectID = require('mongodb').ObjectID
 var passwordHash = require('password-hash')
 var config = require('../config')
+var User = require('../models/User')
 
 exports.postUser = function(request, reply) {
-  MongoClient.connect(config.DATABASE_URL, function(err, db) {
+  var email = request.payload.email
+  var password = request.payload.password
+  var hashedPassword = passwordHash.generate(password)
+
+  var insertCallback = function(err, result) {
     if (err) {
-      reply('An internal server error has occurred.').code(500)
+      console.log(err)
+      return reply('There was a problem with the database').code(500)
     }
+    reply('User successfully added.').code(200)
+  }
 
-    var collection = db.collection('users_soon')
-
-    var email = request.payload.email
-    var password = request.payload.password
-    var hashedPassword = passwordHash.generate(password)
-
-    var insertCallback = function(err, result) {
-      if (err) {
-        reply('An internal server error has occurred').code(500)
-      } else {
-        reply('User successfully added.').code(200)
-      }
-
-      db.close() // Replace this with the user of a promise
+  var resultCallback = function(err, user) {
+    if (err) {
+      console.log(err)
+      return reply('There was a problem with the database').code(500)
     }
-
-    var resultCallback = function(err, user) {
-      if (err) {
-        reply('An error has occurred while removing the user.').code(500)
-        db.close()
-      } else {
-        if (user) {
-          reply('A user account with this email address already exists.').code(500)
-          db.close() // Replace this with the user of a promise
-        } else {
-          collection.insert({
-            email: email,
-            password_hashed: hashedPassword,
-            timestamp: Math.round(Date.now() / 1000)
-          }, insertCallback)
-        }
-      }
+    if (user) {
+      reply('A user account with this email address already exists.').code(500)
+    } else {
+      var newUser = new User({
+        email: email,
+        password: hashedPassword,
+        timestamp: Math.round(Date.now() / 1000),
+        ip: request.info.remoteAddress
+      })
+      newUser.save(insertCallback)
     }
+  }
 
-    collection.findOne({
-      email: email
-    }, resultCallback)
-  })
+  User.findOne({
+    email: email
+  }, resultCallback)
 }
 
 exports.postLogin = function(request, reply) {
