ae1cbaccc63dd0c98571616097d17bf3702227fd
James Brooking <jambroo@gmail.com>
2015-05-25 13:10:56 +0000

    Config?

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,7 @@
 if(process.env.NODE_ENV == 'production') {
   exports.DATABASE_URL = 'mongodb://blackbeard:dgf4e6lyGs6Ck7jwEkcXKC73JJY@ds037611.mongolab.com:37611/blackbeard'; 
+//} else if(process.env.NODE_ENV == 'test') {
+  // Something like this?
 } else {
   exports.DATABASE_URL = 'mongodb://' + process.env.DB_PORT_27017_TCP_ADDR + ':' +process.env.DB_PORT_27017_TCP_PORT + '/blackbeard';
 }
