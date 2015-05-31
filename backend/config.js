f0af0a9f5ecedc98863483d974519c02328ec1bd
James Brooking <jambroo@gmail.com>
2015-05-31 12:30:06 +0000

    Mailgun config.

--- backend/config.js
+++ backend/config.js
@@ -7,3 +7,8 @@ if(process.env.NODE_ENV == 'production') {
 }
 
 exports.AUTH_SECRET = 'wuzUyxdXbqTjMgAK8BjdpGy3zaqGrSv2pwj2pbwq'
+
+exports.MAILGUN = {
+  key: 'key-5859ef062599d1fd5412c49e413f2e26',
+  domain: 'sandbox7790b5e2eec7461d9501b24fa25d8d54.mailgun.org'
+}
