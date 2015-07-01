3024e5966b7c7e146cb119e0e6d67363508025f2
Kevin Simper <kevin.simper@gmail.com>
2015-07-01 18:09:31 +0200

    Closes #50 make the show page load data
    Add moment.js for parsing and formatting dates

--- frontend/app/controlpanel/Routes/App/Show.jsx
+++ frontend/app/controlpanel/Routes/App/Show.jsx
@@ -1,10 +1,44 @@
 var React = require('react')
+var extend = require('lodash/object/extend')
+var AppStore = require('./Store')
+var Actions = require('./Actions')
+var moment = require('moment')
 
 var Show = React.createClass({
+  getState: function() {
+    return {
+      app: AppStore.getOneApp(this.props.params.id)
+    }
+  },
+  getInitialState: function() {
+    return extend(this.getState(), {
+      loaded: false
+    })
+  },
+  componentDidMount: function() {
+    var self = this
+    Actions.load()
+    .then(function() {
+      self.setState({
+        loaded: true
+      })
+    })
+    this.unsubscribe = AppStore.listen(this.onChange)
+  },
+  componentWillUnmount: function() {
+    this.unsubscribe()
+  },
+  onChange: function() {
+    this.setState(this.getState())
+  },
   render: function() {
+    if(!this.state.loaded) {
+      return <div>Loading ...</div>
+    }
     return (
       <div>
-        <h1>App {this.props.params.id}</h1>
+        <h1>{this.state.app.name}</h1>
+        <div>Created: {moment(parseInt(this.state.app.timestamp) * 1000).format()}</div>
       </div>
     );
   }
