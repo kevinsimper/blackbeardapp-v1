6041e5ce95eaa3990d3a96a9fb0d4f78a33d0727
Kevin Simper <kevin.simper@gmail.com>
2015-07-06 14:52:54 +0200

    Load in fixture, so that there is a user to begin with

---
+++ backend/tests/creditcard.js
@@ -0,0 +1,73 @@
+var Lab = require('lab')
+var lab = exports.lab = Lab.script()
+var request = require('request')
+var expect = require('unexpected')
+
+var helpers = require('./helpers/')
+var appUrl = helpers.appUrl()
+
+var server = require('../server')
+server.start(function() {
+  console.log('Server running at:', server.info.uri)
+})
+
+lab.experiment('/users/{id}/creditcards', function() {
+  var token = null
+  lab.before(function(done) {
+    request({
+        method: 'POST',
+        uri: appUrl + '/login',
+        json: true,
+        body: {
+          email: 'admin+users@blackbeard.io',
+          password: 'password'
+        }
+      },
+      function(error, response, body) {
+        token = body.token
+        done()
+      })
+  })
+
+  lab.test('POST', function(done) {
+      var requestData = {
+        name: 'New Card',
+        creditcard: '4111111111111111',
+        expiryMonth: '06',
+        expiryYear: '2018',
+        cvv: '123'
+      }
+      request({
+        method: 'POST',
+        uri: appUrl + '/users/me/creditcards',
+        headers: {
+          'Authorization': token
+        },
+        json: true,
+        body: requestData
+      },
+      function(error, response, body) {
+        expect(response.statusCode, 'to be', 200)
+        done()
+      })
+  })
+
+  lab.test('DELETE', function(done) {
+      var requestData = {
+        name: 'New Card'
+      }
+      request({
+        method: 'DELETE',
+        uri: appUrl + '/users/me/creditcards/' + requestData.name,
+        headers: {
+          'Authorization': token
+        },
+        json: true,
+        body: requestData
+      },
+      function(error, response, body) {
+        expect(response.statusCode, 'to be', 200)
+        done()
+      })
+  })
+})
