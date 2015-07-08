65983b1bd324f353b2cde45ed6c010b2ef63c207
Kevin Simper <kevin.simper@gmail.com>
2015-07-08 16:41:45 +0200

    Listen also on jsx files
    Going to be change later as .jsx file ending sucks

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -76,7 +76,7 @@ gulp.task('sass', function() {
 
 gulp.task('build', ['browserify', 'browserify-controlpanel', 'sass']);
 gulp.task('default', ['browserify', 'browserify-controlpanel', 'sass'], function() {
-  gulp.watch(['./app/*.js'], ['browserify']);
+  gulp.watch(['./app/*.js', './app/*.jsx'], ['browserify']);
   gulp.watch(['./app/controlpanel/**/*.js', './app/controlpanel/**/*.jsx'], ['browserify-controlpanel']);
   gulp.watch(['./app/controlpanel/**/*.scss'], ['sass']);
 });
