8608279b8c9e22369cdd5b9688b8de9360a679ca
James Brooking <jambroo@gmail.com>
2015-07-15 13:16:28 +0000

    Self signed certificate now being used for registry proxy.

--- registry-proxy/server.js
+++ registry-proxy/server.js
@@ -1,8 +1,13 @@
 var Hapi = require('hapi')
+var Fs = require('fs')
 var server = new Hapi.Server()
 
 server.connection({
-    port: '9500'
+    port: '9500',
+    tls: {
+        key: Fs.readFileSync(__dirname + 'registry.blackbeard.dev.key', 'utf8'),
+        cert: Fs.readFileSync(__dirname + 'registry.blackbeard.dev.crt', 'utf8')
+    }
 });
 
 server.route({
@@ -14,6 +19,15 @@ server.route({
       reply('!');
     }
   }
+});
+
+server.ext('onRequest', function (request, reply) {
+    if (request.headers['x-forwarded-proto'] === 'http') {
+        console.log('redirect to https');
+        return reply().redirect('https://' + request.headers.host + request.url.path).code(301);
+    }
+    console.log('NO redirect');
+    return reply.continue();
 });
 
 module.exports = server
