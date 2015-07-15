fd1865c105a9b7c81f4389c50520f011d0e74369
Kevin Simper <kevin.simper@gmail.com>
2015-07-15 11:38:36 +0200

    Remove harp as a dependency
    It was causing troubles, because it was including a native module inside
    Also only include newRelic if in production
    Install jade to compile templates

--- frontend/server.js
+++ frontend/server.js
@@ -1,14 +1,25 @@
-require('newrelic')
+if(process.env.NODE_ENV === 'production') {
+  require('newrelic')
+}
 
-var express = require('express');
-var harp = require('harp');
-var app = express();
+var express = require('express')
+var app = express()
 
-var BACKEND_HOST = process.env.BACKEND_HOST;
+var BACKEND_HOST = process.env.BACKEND_HOST
 
-app.use(harp.mount(__dirname + '/public'));
+app.set('view engine', 'jade')
+app.set('views', './views')
+app.use(express.static('public'))
+
+app.get('/', function(req, res) {
+  res.render('index')
+})
+
+app.get('/controlpanel', function(req, res) {
+  res.render('controlpanel/index')
+})
 
 var port = process.env.PORT || 9000
 app.listen(port, function() {
-  console.log('Frontend started listening on ' + port);
-});
+  console.log('Frontend started listening on ' + port)
+})
