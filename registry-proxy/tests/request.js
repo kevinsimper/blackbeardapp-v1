e5286c087598dd162a77334bb334f22323136428
Kevin Simper <kevin.simper@gmail.com>
2015-07-16 15:22:28 +0200

    A test that shows that the proxy works

--- registry-proxy/tests/request.js
+++ registry-proxy/tests/request.js
@@ -1,8 +1,19 @@
 var expect = require('unexpected')
 var Lab = require('lab')
 var lab = exports.lab = Lab.script()
+var server = require('../server')
+var request = require('request')
+
+server.start(function() {
+  console.log('Server running at:', server.info.uri)
+})
 
 lab.test('returns true when 1 + 1 equals 2', function (done) {
-    expect(1 + 1, 'to be', 2)
+  request.get({
+    url: 'https://localhost:9500',
+    rejectUnauthorized: false
+  }, function(error, response, body) {
+    expect(response.statusCode, 'to be', 200)
     done()
+  })
 })
