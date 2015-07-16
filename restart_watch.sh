8560183e52a791a61e8d6f893d97e14272e49732
Kevin Simper <kevin.simper@gmail.com>
2015-07-16 15:02:19 +0200

    Make the restart_watch work with both proxy and backend

--- restart_watch.sh
+++ restart_watch.sh
@@ -1,1 +1,1 @@
-nodemon --exec "docker-compose stop -t 0 backend && docker-compose start backend && docker-compose logs backend" -w backend/
+nodemon --exec "docker-compose stop -t 0 $1 && docker-compose start $1 && docker-compose logs $1" -w $2/
