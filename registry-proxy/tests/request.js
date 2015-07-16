2fafac14e5003e5d14d31d64e86a8ea1fc0313df
Kevin Simper <kevin.simper@gmail.com>
2015-07-16 16:13:17 +0200

    Get output of real registry to test that it is online

--- registry-proxy/tests/request.js
+++ registry-proxy/tests/request.js
@@ -3,6 +3,7 @@ var Lab = require('lab')
 var lab = exports.lab = Lab.script()
 var server = require('../server')
 var request = require('request')
+var child_process = require('child_process')
 
 server.start(function() {
   console.log('Server running at:', server.info.uri)
@@ -14,6 +15,21 @@ lab.test('returns true when 1 + 1 equals 2', function (done) {
     rejectUnauthorized: false
   }, function(error, response, body) {
     expect(response.statusCode, 'to be', 200)
+    done()
+  })
+})
+
+lab.test('get registry output', function(done) {
+  var ip = child_process.execSync('/sbin/ip route|awk \'/default/ { print $3 }\'', {
+    encoding: 'utf8'
+  })
+  var url = 'http://' + ip.trim() + ':5000/v2/'
+  request.get({
+    url: url,
+    json: true
+  }, function(error, response, body) {
+    expect(response.statusCode, 'to be', 200)
+    expect(typeof body, 'to be', typeof {})
     done()
   })
 })
