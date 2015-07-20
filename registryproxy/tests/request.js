b0d2846897d5cc6c9c93a9a25a6305334e0cce13
Kevin Simper <kevin.simper@gmail.com>
2015-07-20 18:38:34 +0200

    Start the server in a before task

--- registryproxy/tests/request.js
+++ registryproxy/tests/request.js
@@ -5,22 +5,26 @@ var server = require('../server')
 var request = require('request')
 var child_process = require('child_process')
 
-console.log('Server running at:', server.info.uri)
 var ip = child_process.execSync('/sbin/ip route|awk \'/default/ { print $3 }\'', {
   encoding: 'utf8'
 })
 
-lab.test('get registry output', function(done) {
+lab.before(function(done) {
   server.start(function() {
-    var url = 'http://' + ip.trim() + ':5000/v2/'
-    request.get({
-      url: url,
-      json: true
-    }, function(error, response, body) {
-      expect(response.statusCode, 'to be', 200)
-      expect(typeof body, 'to be', typeof {})
-      done()
-    })
+    console.log('Server running at:', server.info.uri)
+    done()
+  })
+})
+
+lab.test('get registry output', function(done) {
+  var url = 'http://' + ip.trim() + ':5000/v2/'
+  request.get({
+    url: url,
+    json: true
+  }, function(error, response, body) {
+    expect(response.statusCode, 'to be', 200)
+    expect(typeof body, 'to be', typeof {})
+    done()
   })
 })
 
