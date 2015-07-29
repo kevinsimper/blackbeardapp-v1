75890728bff4302d3f0154e9e8454a78a6fb0739
James Brooking <jambroo@gmail.com>
2015-07-29 16:24:54 +0200

    Testing out loggly

--- backend/start.js
+++ backend/start.js
@@ -1,9 +1,32 @@
+var options = {}
+
 if(process.env.NODE_ENV === 'production') {
   require('newrelic')
+
+  options = {
+    opsInterval: 1000,
+    reporters: [{
+      reporter: require('good-loggly'),
+      events: { log: '*', request: '*'},
+      config: {
+        token     : process.env.LOGLY_ENV_TOKEN,
+        subdomain : process.env.LOGLY_ENV_SUBDOMAIN
+      }
+    }]
+  }
 }
 
 var server = require('./server')
 
-server.start(function() {
-  console.log('Server running at:', server.info.uri)
+server.register({
+  register: require('good'),
+  options: options
+}, function (err) {
+  if (err) {
+    console.error(err);
+  } else {
+    server.start(function() {
+      console.log('Server running at:', server.info.uri)
+    })
+  }
 })
