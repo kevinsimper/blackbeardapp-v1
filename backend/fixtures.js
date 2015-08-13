d7193a83231a866c1120c873dc90a34fa766a2f4
James Brooking <jambroo@gmail.com>
2015-08-13 10:43:42 +0200

    Added regular user to fixtures.

--- backend/fixtures.js
+++ backend/fixtures.js
@@ -1,14 +1,4 @@
 use blackbeard;
-db.apps.insert({
-  "_id": ObjectId("559396bf05974b0c00b6b284"),
-  "name": "testapp",
-  "user": ObjectId("559396be05974b0c00b6b281"),
-  "timestamp": "1435735744",
-  "containers": [],
-  "image": ObjectId("555cb1e2fc27fe6f5f543901"),
-  "deleted": false,
-  "__v": 0
-});
 db.preusers.insert({
   "_id": ObjectId("559396bf05974b0c00b6b283"),
   "email": "admin+signup@blackbeard.io",
@@ -41,13 +31,28 @@ db.users.insert({
   "deleted": false,
   "__v": 0
 });
-db.supports.insert({
-  "_id": ObjectId("555cb1e2fc27fe6f5f5439ff"),
-  "name": "Contact",
-  "email": "admin+contact@blackbeard.io",
-  "message": "This is a test message.",
-  "timestamp": "1432138210",
+db.users.insert({
+  "_id": ObjectId("559396be05974b0c00b6b282"),
+  "email": "user@blackbeard.io",
+  "password": "sha1$5474e86b$1$0d184fe27ec8573b8f34cf5b33e9a7a237fe5b27",
+  "name": "User One",
+  "username": "user01",
+  "credit": 1000,
+  "timestamp": "1435735744",
   "ip": "127.0.0.1",
+  "role": "USER",
+  "creditCards": [],
+  "deleted": false,
+  "__v": 0
+});
+db.apps.insert({
+  "_id": ObjectId("559396bf05974b0c00b6b284"),
+  "name": "testapp",
+  "user": ObjectId("559396be05974b0c00b6b282"),
+  "timestamp": "1435735744",
+  "containers": [],
+  "image": ObjectId("555cb1e2fc27fe6f5f543901"),
+  "deleted": false,
   "__v": 0
 });
 db.images.insert({
@@ -55,7 +60,16 @@ db.images.insert({
   "name": "Example Image",
   "createdAt": "1432138210",
   "modifiedAt": "1432138211",
-  "user": ObjectId("559396be05974b0c00b6b281"),
+  "user": ObjectId("559396be05974b0c00b6b282"),
   "logs": [{timestamp: "1435735743"}],
+  "__v": 0
+});
+db.supports.insert({
+  "_id": ObjectId("555cb1e2fc27fe6f5f5439ff"),
+  "name": "Contact",
+  "email": "admin+contact@blackbeard.io",
+  "message": "This is a test message.",
+  "timestamp": "1432138210",
+  "ip": "127.0.0.1",
   "__v": 0
 });
