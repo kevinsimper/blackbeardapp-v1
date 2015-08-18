ff15595dc7ae16bde1867d209cb831c81795283f
James Brooking <jambroo@gmail.com>
2015-08-18 12:59:16 +0200

    Fixing rounding issue.

--- backend/tests/billing.js
+++ backend/tests/billing.js
@@ -13,10 +13,10 @@ lab.experiment('Testing Billing service', function() {
     var end = moment('2015-09', "YYYY-MM")
 
     var containers = [
-      new Container({createdAt: moment('2015-07-24 18:31:12').unix(), deletedAt: '2015-08-24 18:31:12'}), // 570h
-      new Container({createdAt: moment('2015-07-24 18:31:12').unix(), deletedAt: '2015-10-24 18:31:12'}), // 744h
+      new Container({createdAt: moment('2015-07-24 18:31:12').unix(), deletedAt: '2015-08-24 18:01:12'}), // 571h
+      new Container({createdAt: moment('2015-07-24 18:01:12').unix(), deletedAt: '2015-10-24 18:31:12'}), // 744h
       new Container({createdAt: moment('2015-01-24 18:31:12').unix(), deletedAt: '2015-01-24 18:31:12'}), // 0h
-      new Container({createdAt: moment('2015-08-06 18:31:12').unix(), deletedAt: '2015-10-24 18:31:12'}), // 605h
+      new Container({createdAt: moment('2015-08-06 18:31:12').unix(), deletedAt: '2015-10-24 18:31:12'}), // 606h
     ]
     var app = new App({name: "testApp"})
     var test = app.toObject()
@@ -24,16 +24,16 @@ lab.experiment('Testing Billing service', function() {
 
     Billing.getAppBillableHours(test, start, end).then(function(hours) {
       var total = 0
-      expect(moment('2015-08-24 18:31:12').diff('2015-08-01 00:00', 'hour'), 'to be', 570)
-      total += 570
-      expect(moment('2015-09-01 00:00:00').diff('2015-08-01 00:00', 'hour'), 'to be', 744)
+      expect(Math.ceil(moment('2015-08-24 18:01:12').diff('2015-08-01 00:00', 'minute')/60.0), 'to be', 571)
+      total += 571
+      expect(Math.ceil(moment('2015-09-01 00:00:00').diff('2015-08-01 00:00', 'minute')/60.0), 'to be', 744)
       total += 744
       // Skipping old app
-      expect(moment('2015-09-01 00:00').diff('2015-08-06 18:31:12', 'hour'), 'to be', 605)
-      total += 605
-      expect(hours, 'to be', 1919)
+      expect(Math.ceil(moment('2015-09-01 00:00').diff('2015-08-06 18:31:12', 'minute')/60.0), 'to be', 606)
+      total += 606
+      expect(hours, 'to be', 1921)
 
-      expect(total, 'to be', 1919)
+      expect(total, 'to be', 1921)
 
       done()
     })
