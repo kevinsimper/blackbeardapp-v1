3c4aa309c79af8f00d14d5a7178435f6c6578957
Kevin Simper <kevin.simper@gmail.com>
2015-08-20 14:04:37 +0200

    Add Clusters page to the controlpanel

---
+++ frontend/app/controlpanel/components/Clusters/index.js
@@ -0,0 +1,13 @@
+var React = require('react')
+
+var Clusters = React.createClass({
+  render: function () {
+    return (
+      <div className='Clusters'>
+        <h1>Clusters</h1>
+      </div>
+    )
+  }
+})
+
+module.exports = Clusters
