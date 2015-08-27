a68b8662c7ddc19af0f7fd184092799689ac69eb
Kevin Simper <kevin.simper@gmail.com>
2015-08-27 14:09:22 +0200

    Show the containers running in a table

--- frontend/app/controlpanel/components/ClusterContainers/index.js
+++ frontend/app/controlpanel/components/ClusterContainers/index.js
@@ -1,11 +1,13 @@
 var React = require('react')
 var request = require('superagent')
 var config = require('../../config')
+var Table = require('../Table')
+var Snippet = require('../Snippet')
 
 var ClusterContainers = React.createClass({
   getInitialState: function () {
     return {
-      containers: {}
+      containers: []
     }
   },
   componentDidMount: function () {
@@ -22,8 +24,41 @@ var ClusterContainers = React.createClass({
     return (
       <div className='ClusterContainers'>
         <h2>Containers</h2>
+        <Table>
+          <thead>
+            <tr>
+              <th>Image</th>
+              <th>Command</th>
+              <th>Created</th>
+              <th>Status</th>
+              <th>Ports</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.containers.map(function (item) {
+              return (
+                <tr>
+                  <td>{item.Image}</td>
+                  <td>{item.Command}</td>
+                  <td>{item.Created}</td>
+                  <td>{item.Status}</td>
+                  <td>
+                    {item.Ports.map(function (port) {
+                      console.log(port)
+                      return <div>{port.IP}:{port.PublicPort}-&gt;{port.PrivatePort}, </div>
+                    })}
+                  </td>
+                  <td>{item.Names[0]}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </Table>
         <pre>
-          {JSON.stringify(this.state.containers, null, 2)}
+          <Snippet>
+            {JSON.stringify(this.state.containers, null, 2)}
+          </Snippet>
         </pre>
       </div>
     )
