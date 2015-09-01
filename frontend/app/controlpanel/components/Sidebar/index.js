e0959517f87601e276f8ea615d51d88f8a2e01c8
Kevin Simper <kevin.simper@gmail.com>
2015-09-01 18:17:13 +0200

    Hide admin menu items from the user

--- frontend/app/controlpanel/components/Sidebar/index.js
+++ frontend/app/controlpanel/components/Sidebar/index.js
@@ -1,8 +1,22 @@
 var React = require('react');
 var auth = require('../../auth')
 var Button = require('../Button/')
+var ProfileActions = require('../Profile/actions')
 
 var Sidebar = React.createClass({
+  getInitialState: function () {
+    return {
+      profile: {}
+    }
+  },
+  componentDidMount: function () {
+    var self = this
+    ProfileActions.load().then(function (profile) {
+      self.setState({
+        profile: profile
+      })
+    })
+  },
   render: function() {
     return (
       <div className='Sidebar'>
@@ -16,27 +30,31 @@ var Sidebar = React.createClass({
           <div className='Sidebar__Item'>
             <a className='Sidebar__Link' href="#/profile">My Profile</a>
           </div>
-          <div className='Sidebar__Item'>
-            <a className='Sidebar__Link' href="#/images">Images</a>
-          </div>
-          <div className='Sidebar__Item'>
-            <a className='Sidebar__Link' href="#/preusers">PreUsers</a>
-          </div>
-          <div className='Sidebar__Item'>
-            <a className='Sidebar__Link' href="#/users">Users</a>
-          </div>
-          <div className='Sidebar__Item'>
-            <a className='Sidebar__Link' href="#/billing">Billing</a>
-          </div>
-          <div className='Sidebar__Item'>
-            <a className='Sidebar__Link' href="#/logs">Logs</a>
-          </div>
-          <div className='Sidebar__Item'>
-            <a className='Sidebar__Link' href="#/clusters">Clusters</a>
-          </div>
-          <div className='Sidebar__Item'>
-            <a className='Sidebar__Link' href="#/vouchers">Vouchers</a>
-          </div>
+          {this.state.profile.role && (this.state.profile.role === 'ADMIN') &&
+            <div>
+              <div className='Sidebar__Item'>
+                <a className='Sidebar__Link' href="#/images">Images</a>
+              </div>
+              <div className='Sidebar__Item'>
+                <a className='Sidebar__Link' href="#/preusers">PreUsers</a>
+              </div>
+              <div className='Sidebar__Item'>
+                <a className='Sidebar__Link' href="#/users">Users</a>
+              </div>
+              <div className='Sidebar__Item'>
+                <a className='Sidebar__Link' href="#/billing">Billing</a>
+              </div>
+              <div className='Sidebar__Item'>
+                <a className='Sidebar__Link' href="#/logs">Logs</a>
+              </div>
+              <div className='Sidebar__Item'>
+                <a className='Sidebar__Link' href="#/clusters">Clusters</a>
+              </div>
+              <div className='Sidebar__Item'>
+                <a className='Sidebar__Link' href="#/vouchers">Vouchers</a>
+              </div>
+            </div>
+          }
           <div className='Sidebar__Item' style={{padding: 10}}>
             <Button onClick={auth.logout}>Log out</Button>
           </div>
