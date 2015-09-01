3afbe308e0941cb2a9bb16818bfda5d547d23d8f
Kevin Simper <kevin.simper@gmail.com>
2015-09-01 14:54:22 +0200

    Replace with env variables

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production') {
-  exports.DATABASE_URL = 'mongodb://blackbeard:dgf4e6lyGs6Ck7jwEkcXKC73JJY@ds037611.mongolab.com:37611/blackbeard'
+  exports.DATABASE_URL = process.env.DATABASE_URL
 } else if(process.env.NODE_ENV === 'development' && (typeof process.env.DATABASE_URL !== 'undefined')) {
   exports.DATABASE_URL = process.env.DATABASE_URL
 } else {
@@ -7,7 +7,7 @@ if(process.env.NODE_ENV == 'production') {
 }
 
 if(process.env.NODE_ENV == 'production') {
-  exports.RABBITMQ_URL = 'mongodb://blackbeard:dgf4e6lyGs6Ck7jwEkcXKC73JJY@ds037611.mongolab.com:37611/blackbeard'
+  exports.RABBITMQ_URL = process.env.RABBITMQ_URL
 } else if(process.env.NODE_ENV === 'development' && (typeof process.env.RABBITMQ_URL !== 'undefined')) {
   exports.RABBITMQ_URL = process.env.RABBITMQ_URL
 } else {
