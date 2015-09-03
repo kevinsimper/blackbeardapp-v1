b0fa988de8e133f29e72d1e070340b42a92c4431
James Brooking <jambroo@gmail.com>
2015-09-03 17:10:21 +0200

    Convenience script to completely tear down and start up cluster with one node.

---
+++ swarm/preup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SWARM_ID=$(docker run --rm swarm create)
+echo $SWARM_ID
+
+docker-machine create \
+-d virtualbox \
+--swarm \
+--swarm-master \
+--swarm-discovery token://$SWARM_ID \
+--engine-insecure-registry registry.blackbeard.dev:9500 \
+swarm-master
