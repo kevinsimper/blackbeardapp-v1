04645dbb3c04c849e553d47487b080e482dd4b94
Kevin Simper <kevin.simper@gmail.com>
2015-09-10 14:00:11 +0200

    When zipping files, don't accidentally zip the ignored testpayload.json

--- cron/upload.sh
+++ cron/upload.sh
@@ -1,2 +1,2 @@
 zip -r billing.zip billing/
-iron worker upload --zip billing.zip -name cron-billing iron/images:node-0.12 sh -c 'node request -payload testpayload.json'
+iron worker upload --zip billing.zip -name cron-billing iron/images:node-0.12 sh -c 'node request'
