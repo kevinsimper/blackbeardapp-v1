ba987d64ca4148df0b2a96469b28deb7b358fe70
Kevin Simper <kevin.simper@gmail.com>
2015-09-15 14:20:39 +0200

    The worker need to know where the backend is located at

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,8 @@
+var child_process = require('child_process')
+var ip = child_process.execSync('/sbin/ip route|awk \'/default/ { print $3 }\'', {
+  encoding: 'utf8'
+}).trim()
+
 if(process.env.NODE_ENV == 'production') {
   exports.DATABASE_URL = process.env.DATABASE_URL
 } else if(process.env.NODE_ENV === 'development' && (typeof process.env.DATABASE_URL !== 'undefined')) {
@@ -27,10 +32,6 @@ if(process.env.NODE_ENV == 'production') {
 } else if(process.env.NODE_ENV === 'development' && (typeof process.env.REGISTRY_FULLURL !== 'undefined')) {
   exports.REGISTRY_FULLURL = process.env.REGISTRY_FULLURL
 } else {
-  var child_process = require('child_process')
-  var ip = child_process.execSync('/sbin/ip route|awk \'/default/ { print $3 }\'', {
-    encoding: 'utf8'
-  }).trim()
   exports.REGISTRY_FULLURL = 'http://' + ip + ':5000'
 }
 
@@ -41,4 +42,12 @@ if(process.env.NODE_ENV == 'production') {
   exports.WORKER_PASSWORD = process.env.WORKER_PASSWORD
 } else {
   exports.WORKER_PASSWORD = 'development'
+}
+
+if(process.env.NODE_ENV == 'production') {
+  exports.BACKEND_URL = process.env.BACKEND_URL
+} else if(process.env.NODE_ENV === 'development' && (typeof process.env.BACKEND_URL !== 'undefined')) {
+  exports.BACKEND_URL = process.env.BACKEND_URL
+} else {
+  exports.BACKEND_URL = 'http://' + ip + ':8000'
 }
