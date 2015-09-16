bf820d366571f16957d78fddab1cadd17ee8e04b
Kevin Simper <kevin.simper@gmail.com>
2015-09-16 19:20:30 +0200

    New-relic for router

--- router/server.js
+++ router/server.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV == 'production') {
+  require('newrelic')
+}
+
 var debug = require('debug')('router')
 var Promise = require('bluebird')
 var request = Promise.promisify(require('request'))
