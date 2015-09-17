222fffe26f61f5229ff05b7e762c8571d6b594a0
James Brooking <jambroo@gmail.com>
2015-09-17 10:21:30 +0200

    Resolving conflict.

--- frontend/app/controlpanel/components/AppShow/index.js
+++ frontend/app/controlpanel/components/AppShow/index.js
@@ -8,15 +8,19 @@ var AppsStore = require('../Apps/store')
 var AppsActions = require('../Apps/actions')
 var AppLogs = require('../AppLogs/')
 var Containers = require('../Containers/')
+var ContainerActions = require('../Containers/actions')
+var ContainerStore = require('../Containers/store')
 var Button = require('../Button/')
 var StatusIcon = require('../StatusIcon/')
 var TimeSince = require('../TimeSince/')
+var filter = require('lodash/collection/filter')
 
 var AppShow = React.createClass({
   mixins: [Navigation],
   getState: function() {
     return {
-      app: AppsStore.getOneApp(this.props.params.id)
+      app: AppsStore.getOneApp(this.props.params.id),
+      containers: ContainerStore.getOne(this.props.params.id)
     }
   },
   getInitialState: function() {
@@ -32,11 +36,19 @@ var AppShow = React.createClass({
           loaded: true
         })
       })
+    ContainerActions.loadOne(this.props.app)
+      .then(function() {
+        self.setState({
+          loaded: true
+        })
+      })
 
     this.unsubscribe = AppsStore.listen(this.onChange)
+    this.unsubscribeContainers = ContainerStore.listen(this.onChange)
   },
   componentWillUnmount: function() {
     this.unsubscribe()
+    this.unsubscribeContainers()
   },
   onChange: function() {
     this.setState(this.getState())
@@ -60,6 +72,8 @@ var AppShow = React.createClass({
       return <div>Loading ...</div>
     }
 
+    var runningContainers = filter(this.state.containers, {deleted: false}) || []
+
     return (
       <div className='AppShow'>
         <h1><StatusIcon/>{this.state.app.name}</h1>
@@ -68,8 +82,10 @@ var AppShow = React.createClass({
           <TimeSince timestamp={this.state.app.timestamp}/>
         </div>
         <div>
-          <Button onClick={this.onClickStart}>Start containers</Button>
-          <Button variant='danger' onClick={this.onClickDelete}>Delete</Button>
+          <Button onClick={this.onClickStart}>Start Container</Button>
+          {runningContainers.length === 0 &&
+            <Button variant='danger' onClick={this.onClickDelete}>Delete App</Button>
+          }
         </div>
         <Containers app={this.state.app._id} />
         <AppLogs app={this.state.app._id} />
