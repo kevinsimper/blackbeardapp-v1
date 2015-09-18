5cd7633f1f980b61b8056bbf6d3aa0a4840324c8
Kevin Simper <kevin.simper@gmail.com>
2015-09-18 12:42:04 +0200

    Improve speed of restart_watch

--- restart_watch.sh
+++ restart_watch.sh
@@ -1,1 +1,8 @@
-nodemon --exec "docker-compose stop -t 0 $1 && docker-compose start $1 && docker-compose logs $1" -w $1/
+nodemon --exec " \
+  docker-compose stop -t 0 $1 && \
+  docker-compose start $1 && \
+  docker logs --tail=10 -f \
+    $(docker-compose ps | \
+      awk '{print $1}' | \
+      grep $1)" \
+  -w $1/
