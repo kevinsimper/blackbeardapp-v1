f1abf9cddcabf127563f71963397ea970bc0cf00
Kevin Simper <kevin.simper@gmail.com>
2015-09-18 13:28:38 +0200

    Fix big bug where it did not find the correct cluster but just a random one
    To delete a container, we should find the cluster that the container
    is located on and then delete it from there.
    
    Closes #416

--- backend/worker.js
+++ backend/worker.js
@@ -160,19 +160,19 @@ Promise.all([mongo, rabbitmq]).then(function () {
   Queue.consume('container-kill', function (message, ack) {
     var ClusterService = require('./services/Cluster')
     var Container = require('./models/Container')
+    var Cluster = require('./models/Cluster')
     var App = require('./models/App')
     var Image = require('./models/Image')
     var User = require('./models/User')
-    var sequest = require('sequest')
 
-    // TODO: Choose proper cluster here
-    var cluster = ClusterService.getCluster()
+    var container = Container.findOne({_id: message.containerId})
 
-    var clusterContainerId = message.containerId
-    var container = Container.findOne({_id: clusterContainerId})
+    var cluster = container.then(function (container) {
+      return Cluster.findOne({_id: container.cluster})
+    })
 
-    cluster.then(function(cluster) {
-      ClusterService.killContainer(cluster, clusterContainerId)
+    Promise.all([cluster, container]).spread(function(cluster, container) {
+      ClusterService.killContainer(cluster, container.containerHash)
       .then(function(result) {
         ack()
       })
