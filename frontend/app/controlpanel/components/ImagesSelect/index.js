dbadc065323c3e75f82c06a9744b87382c7f2b2c
Kevin Simper <kevin.simper@gmail.com>
2015-09-22 00:19:14 +0200

    Only show the latest five updated images

--- frontend/app/controlpanel/components/ImagesSelect/index.js
+++ frontend/app/controlpanel/components/ImagesSelect/index.js
@@ -1,18 +1,27 @@
 var React = require('react')
 var Button = require('../Button/')
+var Input = require('../Input')
 var ImageSelectItem = require('../ImageSelectItem/')
+var filter = require('lodash/collection/filter')
 
 var ImagesSelect = React.createClass({
   onChange: function(imageId) {
     this.props.onChange(imageId)
   },
+  getLatestFive: function () {
+    return filter(this.props.images, function (image) {
+      return image.modifiedAt
+    }).reverse().slice(0, 5)
+  },
   render: function() {
     var self = this
     return (
       <div className='ImagesSelect'>
-        {this.props.images.map(function(image) {
-          return <ImageSelectItem key={image._id} image={image} selected={self.props.value === image._id} onClick={self.onChange.bind(null, image._id)}/>
-        })}
+        <div>
+          {this.getLatestFive().map(function(image) {
+            return <ImageSelectItem key={image._id} image={image} selected={self.props.value === image._id} onClick={self.onChange.bind(null, image._id)}/>
+          })}
+        </div>
       </div>
     )
   }
