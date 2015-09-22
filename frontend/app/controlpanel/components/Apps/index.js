33e5eb3f7afa6ec00a850dc47384b9f4b899c0e7
Kevin Simper <kevin.simper@gmail.com>
2015-09-22 15:04:24 +0200

    Show delete apps in another list

--- frontend/app/controlpanel/components/Apps/index.js
+++ frontend/app/controlpanel/components/Apps/index.js
@@ -2,6 +2,7 @@ var React = require('react')
 var AppStore = require('../Apps/store')
 var AppActions = require('../Apps/actions')
 var StatusIcon = require('../StatusIcon/')
+var filter = require('lodash/collection/filter')
 
 var getState = function() {
   return {
@@ -24,27 +25,47 @@ var AppCreate = React.createClass({
     this.setState(getState())
   },
   render: function() {
+    var activeApps = filter(this.state.apps, {deleted: false}) || []
+    var deletedApps = filter(this.state.apps, {deleted: true}) || []
     return (
       <div className='Apps'>
-        {this.state.apps.map(function(app){
-          return (
-            <div className='Apps__Item'>
-              <a href={'#/apps/' + app._id} className='Apps__Link'>
-                <div className='Apps__StatusIcon'>
-                  <StatusIcon/>
-                </div>
-                <div className='Apps__Info'>
-                  {app.name}
-                  <div className='Apps__Containers'>
-                    <small>
-                      {app.containers.length} containers running
-                    </small>
+        <div className='Apps__List'>
+          {activeApps.map(function(app){
+            return (
+              <div className='Apps__Item'>
+                <a href={'#/apps/' + app._id} className='Apps__Link'>
+                  <div className='Apps__StatusIcon'>
+                    <StatusIcon/>
                   </div>
-                </div>
-              </a>
+                  <div className='Apps__Info'>
+                    {app.name}
+                    <div className='Apps__Containers'>
+                      <small>
+                        {app.containers.length} containers running
+                      </small>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            )
+          })}
+        </div>
+        <div className='Apps__List'>
+          {deletedApps.length !== 0 &&
+            <div>
+              <h2>Deleted Apps</h2>
+              {deletedApps.map(function (app) {
+                return (
+                  <div className='Apps__Item'>
+                    <a href={'#/apps/' + app._id} className='Apps__Link'>
+                      {app.name}
+                    </a>
+                  </div>
+                )
+              })}
             </div>
-          )
-        })}
+          }
+        </div>
       </div>
     );
   }
