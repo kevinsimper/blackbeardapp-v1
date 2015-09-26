80618fb1b130b0b53bb08b2f361b06285b2beaba
James Brooking <jambroo@gmail.com>
2015-09-26 15:29:37 +0200

    Using Promise.map in getRegistryAllImages.

--- backend/routes/registry.js
+++ backend/routes/registry.js
@@ -13,19 +13,19 @@ exports.getRegistryAllImages = {
     var allImages = RegistryService.getAllImages(config.REGISTRY_FULLURL)
 
     var imageTags = allImages.then(function (allImages) {
-      return Promise.all(allImages.map(function (image) {
+      return Promise.map(allImages, function (image) {
         return RegistryService.getOneImage(config.REGISTRY_FULLURL, image)
-      }))
+      })
     }).then(function (data) {
       // Flatten because it should be a collection of objects and not arrays
       return _.flatten(data)
     })
     var imageDetails = imageTags.then(function (imageTags) {
-      return Promise.all(imageTags.map(function (imageTag) {
-        return Promise.all(imageTag.tags.map(function (tag) {
+      return Promise.map(imageTags, function (imageTag) {
+        return Promise.map(imageTag.tags, function (tag) {
           return RegistryService.getOneTagImageManifest(config.REGISTRY_FULLURL, imageTag.name, tag)
-        }))
-      }))
+        })
+      })
     })
 
     Promise.all([allImages, imageTags, imageDetails]).spread(function (allImages, imageTags, imageDetails) {
