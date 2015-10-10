7862021ad6d4fdaa185f1afcec6c1235db1135b7
James Brooking <jambroo@gmail.com>
2015-10-10 06:21:30 +0200

    Clean up.

--- kubernetes/run.sh
+++
@@ -1,1 +0,0 @@
-./kubernetes/cluster/kubectl.sh run my-nginx2 --image=nginx --replicas=1 --port=80
--- kubernetes/vagrantup.sh
+++
@@ -1,8 +0,0 @@
-# Will git clone into a new folder called ./kubernetes
-curl -sS https://get.k8s.io | bash
-
-# then
-# cd kubernetes
-# ./kubernetes/cluster/kubectl.sh get pods
-
-# ./kubernetes/cluster/kubectl.sh run nginx --image=nginx --port=80
