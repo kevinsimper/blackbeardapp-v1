9b8740371c4ac8c840db0c4996b93d23ed88662a
James Brooking <jambroo@gmail.com>
2015-10-10 11:50:05 +0200

    No need to docker pull here.

--- kubernetes/start_container.sh
+++ kubernetes/start_container.sh
@@ -5,9 +5,6 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
-echo "If you don't have $1 pull image."
-sudo docker pull $1
-
 echo "Run $1 on kubernetes cluster"
 ./kubernetes/cluster/kubectl.sh run $1 --image=nginx --replicas=1 --port=80
 
